21c6ff8c4f9905fb38f6eab7d782bc7c30d55830
Serp <manu.serpette74@gmail.com>
2023-11-03 15:23:03 -0400

    path findings

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -15,14 +15,16 @@ def get_unique_value(column_index, array):
 
 
 # custom something to compute the path in the name, to be able to save where we want
-def parse_file(file_name, path="./"):
-    if os.path.exists(path+file_name+"-array.pkl"):
+def parse_file(file_name, path_save="./data/output/temp/"):
+    file_path, file_name = os.path.split(file_name)
+    file_path += "/"
+    if os.path.exists(path_save+file_name+"-array.pkl"):
         # File exists, so load the data from the file
-        with open(path+file_name+"-array.pkl", 'rb') as file:
+        with open(path_save+file_name+"-array.pkl", 'rb') as file:
             array2d = pickle.load(file)
         print("File exists. Data has been loaded.")
     else:
-        file = open(file_name, 'r')
+        file = open(file_path+file_name, 'r')
         file_content = file.read()
         file_content = file_content.split('\n')
         res = []
@@ -34,7 +36,7 @@ def parse_file(file_name, path="./"):
         array = np.array(res, dtype=object)
         array2d = np.vstack(array)
         # File does not exist, so save the data to the file
-        with open(path+file_name+"-array.pkl", 'wb') as file:
+        with open(path_save+file_name+"-array.pkl", 'wb') as file:
             pickle.dump(array2d, file)
         print("File does not exist. Data has been saved.")
     # set is now a 2d array
@@ -50,7 +52,8 @@ def parse_kdd(file_name):
 
 
 if __name__ == '__main__':
-    a = parse_file('../../data/kddcup.data_10_percent')
+    a = parse_file('../../data/kddcup.data_10_percent',
+                   "../../data/output/temp/")
     print(a)
     unique_values = np.unique(a[:, 1])
     print(unique_values)
