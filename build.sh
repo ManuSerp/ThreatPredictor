0ed2e29e2da398a42d1cf31af8a750e0808d0e64
Serp <manu.serpette74@gmail.com>
2023-11-03 14:20:49 -0400

    init

--- build.sh
+++ build.sh
@@ -14,7 +14,12 @@ else
     echo "The directory '$dir' does not exist. Creating it now."
     mkdir "$dir"
 fi
-
+if [ -d "data/output" ]; then
+    echo "The directory 'output' already exists."
+else
+    echo "The directory 'putpt' does not exist. Creating it now."
+    mkdir "data/output"
+fi
 echo "Downloading KDD Cup 1999 dataset..."
 
 wget http://kdd.ics.uci.edu/databases/kddcup99/"$file"
