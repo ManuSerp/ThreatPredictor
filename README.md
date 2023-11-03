46e4ef1f89e5ab62e4d01b959ef06892b7e23d01
Serp <manu.serpette74@gmail.com>
2023-11-03 14:39:06 -0400

    shell new folder

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Install
 
 **build.sh**
-
+This script setup the data folder for running the code.
+Can be safely deleted after running.
 build.sh download the 10% percent dataset.
 Use `-a` option for full dataset
--- build.sh
+++ build.sh
@@ -14,12 +14,30 @@ else
     echo "The directory '$dir' does not exist. Creating it now."
     mkdir "$dir"
 fi
-if [ -d "data/output" ]; then
+if [ -d "$dir/output" ]; then
     echo "The directory 'output' already exists."
 else
     echo "The directory 'putpt' does not exist. Creating it now."
-    mkdir "data/output"
+    mkdir "$dir/output"
 fi
+
+if [ -d "$dir/output/parsing" ]; then
+    echo "The directory 'parsing' already exists."
+else
+    mkdir "$dir/output/parsing"
+fi
+
+if [ -d "$dir/output/models" ]; then
+    echo "The directory 'models' already exists."
+else
+    mkdir "$dir/output/models"
+fi
+if [ -d "$dir/output/temp" ]; then
+    echo "The directory 'temp' already exists."
+else
+    mkdir "$dir/output/temp"
+fi
+
 echo "Downloading KDD Cup 1999 dataset..."
 
 wget http://kdd.ics.uci.edu/databases/kddcup99/"$file"
