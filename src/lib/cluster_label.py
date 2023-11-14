4375b4f2b0097b3cd5b8185a971c92bf05b3ed1f
Serp <manu.serpette74@gmail.com>
2023-11-14 09:54:00 -0500

    cluster_label

---
+++ src/lib/cluster_label.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+
+
+
+
+
+class ClusterLabel:
+    def __init__(self,cluster_affect,label,n_clusters):
+        self.cluster_affect = cluster_affect
+        self.label = label
+        self.cluster_distribution = None
+        self.cluster_label = None
+
+        # unique labels:
+        self.unique_label = np.unique(label)
+
+        self.cluster_distribution = [{}]*self.n_clusters
+        
+
+
+    def cluster_stat(self):
+        for i in range(self.n_clusters):
+            self.cluster_distribution[i] = {label:0 for label in self.unique_label}
+
+        for i in range(len(self.label)):
+            self.cluster_distribution[self.cluster_affect[i]][self.label[i]] += 1
+
+        self.cluster_label = [max(self.cluster_distribution[i], key=self.cluster_distribution[i].get) for i in range(self.n_clusters)]
+
+        
+
+        
+
+        
+
+
+    
