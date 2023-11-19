07420172242868af0175662d420ad66e94802b48
Serp <manu.serpette74@gmail.com>
2023-11-19 15:28:35 -0500

    Meanshift

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 """Main code"""
 import numpy as np
 from models.spectral_clustering import SpectralClusterModel
+from models.mean_shift import MeanShiftModel
 from models.kmean import KMeanModel
 from models.dbscan import DBSCANModel
 from parser.parser import Parser
@@ -16,7 +17,8 @@ def main():
     # Initialize the Preprocess, FeatureReduction and KMeansModel classes
     parser = Parser(PATH_SAVE,LOG_PATH)
     feature_reduction = FeatureReduction('TruncatedSVD')
-    cluster_model = KMeanModel(n_clusters=100, random_state=0)
+    #cluster_model = KMeanModel(n_clusters=100, random_state=0)
+    cluster_model = MeanShiftModel()
     # cluster_model = SpectralClusterModel(n_clusters=2, assign_labels='discretize', random_state=0)
     # cluster_model = DBSCANModel(eps=3, min_samples=2)
 
@@ -40,17 +42,18 @@ def main():
 
     cluster_model.evaluating_clustering_performance(data_out,predict_labels_train)
 
+    print(predict_labels_train)
     # Cluster label
 
-    print("==== Cluster label begining ====")
-    cluster_label = ClusterLabel(predict_labels_train,label_train,cluster_model.n_clusters)
-    cluster_label.cluster_stat()
-    predict_label_test=cluster_model.predict(data_test)
-    pl=cluster_label.get_predicted_label(predict_label_test)
-    res=cluster_label.calc_stat(label_test,pl)
-    cluster_label.plot(res)
+    # print("==== Cluster label begining ====")
+    # cluster_label = ClusterLabel(predict_labels_train,label_train,cluster_model.n_clusters)
+    # cluster_label.cluster_stat()
+    # predict_label_test=cluster_model.predict(data_test)
+    # pl=cluster_label.get_predicted_label(predict_label_test)
+    # res=cluster_label.calc_stat(label_test,pl)
+    # cluster_label.plot(res)
 
-    print(res)
+    # print(res)
 
 
 if __name__ == '__main__':
